b65e09102386b8e3a2498c4761f0fb8596aef443
Mariana Morais <marianaquel.morais@gmail.com>
2021-06-14 18:37:42 -0300

    Adicionar arquivos de projeto.

---
+++ Projeto_Psi/Models/Categoria.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Projeto_Psi.Models
+{
+    public class Categoria {
+        public long CategoriaId { get; set; }
+        public string Nome { get; set; }
+    }
+}
